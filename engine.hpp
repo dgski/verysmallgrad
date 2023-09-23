0400498fe56de30d17f6a8993ddbb270234a4a53
David Gorski <gorski.dave@gmail.com>
2023-09-23 15:41:25 -0400

    Complete Engine work

--- engine.hpp
+++ engine.hpp
@@ -7,11 +7,15 @@
 #include <sstream>
 #include <unordered_set>
 #include <vector>
+#include <cmath>
+#include <memory>
 
 enum class Operation {
   Null,
   Addition,
-  Multiplication
+  Multiplication,
+  Power,
+  RELU
 };
 
 std::string_view toString(Operation op)
@@ -21,6 +25,8 @@ std::string_view toString(Operation op)
     case Operation::Null: return "null";
     case Operation::Addition: return "+";
     case Operation::Multiplication: return "*";
+    case Operation::Power: return "pow";
+    case Operation::RELU: return "RELU";
   }
 
   throw std::runtime_error("Unhandled op");
@@ -30,7 +36,8 @@ struct Value;
 
 struct Inputs {
   Operation operation = Operation::Null;
-  std::vector<Value*> values;
+  std::vector<std::shared_ptr<Value>> values;
+  double power = 0.0;
 };
 
 struct Value {
@@ -49,11 +56,11 @@ struct Value {
 
     visited.insert(value);
     for (auto next : value->_inputs.values) {
-      buildTopo(topo, visited, next);
+      buildTopo(topo, visited, next.get());
     }
     topo.push_back(value);
   }
-public:
+
   Value(double value, Inputs inputs = Inputs{})
   : _value(value), _inputs(inputs)
   {}
@@ -69,8 +76,12 @@ public:
     } else if (_inputs.operation == Operation::Multiplication) {
       auto& a = _inputs.values[0];
       auto& b = _inputs.values[1];
-      a->_grad += b->_value;
-      b->_grad += a->_value;
+      a->_grad += b->_value * _grad;
+      b->_grad += a->_value * _grad;
+    } else if (_inputs.operation == Operation::Power) {
+      _inputs.values[0]->_grad += (_inputs.power * std::pow(_value, _inputs.power-1)) * _grad;
+    } else if (_inputs.operation == Operation::RELU) {
+      _inputs.values[0]->_grad = double(_value > 0.0) * _grad;
     }
   }
 
@@ -82,20 +93,6 @@ public:
     std::for_each(std::rbegin(topo), std::rend(topo), [&](Value* value) {
       value->backwardsOnce();
     });
-  }
-
-  Value operator+(Value& other)
-  {
-    return Value(_value + other._value, Inputs{ Operation::Addition, { this, &other }});
-  }
-  Value operator*(Value& other)
-  {
-    return Value(_value * other._value, Inputs{ Operation::Multiplication, { this, &other }});
-  }
-  friend std::ostream& operator<<(std::ostream& os, const Value& value)
-  {
-    os << value._value;
-    return os;
   }
 
   void printTree(int indents = 0)
@@ -113,5 +110,47 @@ public:
       _inputs.values[1]->printTree(currentStr.size());
     }
   }
+
+  template<typename... Args>
+  static auto make(Args&&... args)
+  {
+    return std::make_shared<Value>(std::forward<Args>(args)...);
+  }
 };
 
+using ValuePtr = std::shared_ptr<Value>;
+
+ValuePtr operator+(ValuePtr a, ValuePtr b)
+{
+  return std::make_shared<Value>(a->_value + b->_value, Inputs{ Operation::Addition, { a, b }});
+}
+ValuePtr operator*(ValuePtr a, ValuePtr b)
+{
+  return std::make_shared<Value>(a->_value * b->_value, Inputs{ Operation::Multiplication, { a, b }});
+}
+ValuePtr power(ValuePtr a, double value)
+{
+  return std::make_shared<Value>(std::pow(a->_value, value), Inputs{ Operation::Power, { a }, value });
+}
+ValuePtr operator-(ValuePtr a)
+{
+  return a * std::make_shared<Value>(-1.0);
+}
+ValuePtr operator/(ValuePtr a, ValuePtr b)
+{
+  return a * power(b, -1.0);
+}
+ValuePtr operator-(ValuePtr a, ValuePtr b)
+{
+  return a + (-b);
+}
+ValuePtr relu(ValuePtr a)
+{
+  return std::make_shared<Value>((a->_value > 0.0 ? a->_value : 0.0), Inputs{ Operation::RELU, { a } });
+}
+
+std::ostream& operator<<(std::ostream& os, const ValuePtr& value)
+{
+  os << value->_value;
+  return os;
+}
--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <iostream>
 
 #include "engine.hpp"
-#include "nn.hpp"
 
 int main() {
-    auto a = Value(2.0);
-    auto b = Value(-3.0);
-    auto c = Value(10.0);
+    auto a = Value::make(2.0);
+    auto b = Value::make(-3.0);
+    auto c = Value::make(10.0);
     auto e = a * b;
     auto d = e + c;
-    auto f = Value(2.0);
+    auto f = Value::make(2.0);
     auto L = d * f;
+    auto lpow = power(L, -1);
 
-    L._grad  = 1.0;
-    L.backwards();
-    L.printTree();
+    auto reluResult= relu(lpow);
+    reluResult->_grad = 1.0;
+    reluResult->backwards();
+    reluResult->printTree();
+
 
     return EXIT_SUCCESS;
 }
